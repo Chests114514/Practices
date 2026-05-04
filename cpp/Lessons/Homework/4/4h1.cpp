1553102161b034af621cf7a14a7291afa02fd30a
Chests114514 <Chests114514@users.noreply.github.com>
2026-05-04 21:57:13 +0200

    050426-2158

---
+++ cpp/Lessons/Homework/4/4h1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main() {
+        using namespace std;
+        string nameFirst;
+        string nameLast;
+        string grade;
+        int age;
+
+        cout << "What is your first name? ";
+        cin >> nameFirst;
+        cout << "What is your last name? ";
+        cin >> nameLast;
+        cout << "What letter grade do you deserve? ";
+        cin >> grade;
+        cout << "What is your age? ";
+        cin >> age;
+
+        if(grade == "A") {
+                grade = "B";
+        } else if(grade == "B") {
+                grade = "C";
+        } else if(grade == "C") {
+                grade = "D";
+        } else if(grade == "D") {
+                grade = "E";
+        } else if(grade == "E") {
+                grade = "F";
+        }
+
+        cout << "Name: " << nameLast << ", " << nameFirst << endl;
+        cout << "Grade: " << grade << endl;
+        cout << "Age: " << age << endl;
+        return 0;
+}
